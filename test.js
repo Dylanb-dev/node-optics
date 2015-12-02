1b896298acd88c7669fb19b6e71662df38596a4a
Dylan <20769605@student.uwa.edu.au>
2015-12-02 14:33:06 +0800

    added signal class

---
+++ test.js
@@ -0,0 +1,21 @@
+'use strict';
+class Polygon {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
+
+
+var x = new Polygon;
+
+x.height = 10;
+
+console.log(x.height);
+
+
+let y = [];
+
+y[0] = 1;
+
+console.log(y[0]);
